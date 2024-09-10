ac9e71b5fea079fba4277a52b38cab2a8b8b4831
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-09-09 22:50:24 -0300

    configuracao inicial do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './component/login/login.component';
+import { CadastroComponent } from './component/cadastro/cadastro.component';
+import { VerificacaoComponent } from './component/verificacao/verificacao.component';
+import { ResetSenhaComponent } from './component/reset-senha/reset-senha.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    VerificacaoComponent,
+    ResetSenhaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/verificacao/verificacao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verificacao',
+  templateUrl: './verificacao.component.html',
+  styleUrls: ['./verificacao.component.css']
+})
+export class VerificacaoComponent {
+
+}
